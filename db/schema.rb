d3f5ab2e9ebc1a1cb273d7f2c26865e2a9369537
IlkhamGaysin <ilkham.gaysin@flatstack.com>
2016-11-24 14:27:36 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124111523) do
+ActiveRecord::Schema.define(version: 20161124111937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "room_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id", using: :btree
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title",      null: false
@@ -43,4 +51,5 @@ ActiveRecord::Schema.define(version: 20161124111523) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "messages", "rooms"
 end
